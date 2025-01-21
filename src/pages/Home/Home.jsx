d86fc2b96829521c55320ec3ea180fb4ce4d8db9
Nusrat Jahan <140106780+nusrat40@users.noreply.github.com>
2025-01-21 18:32:29 +0600

    added tutor section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import { Helmet } from 'react-helmet-async';
+import TutorSection from './TutorSection';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
                 <title>iLearning | Home</title>
             </Helmet>
             <Banner></Banner>
+            <TutorSection></TutorSection>
         </div>
     );
 };
---
+++ src/pages/Home/TutorSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+
+const TutorSection = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: tutors = [], refetch } = useQuery({
+    queryKey: ["tutors"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/users/tutors");
+      return res.data;
+    },
+  });
+
+  return (
+    <div className="space-y-10 my-20">
+      <h1 className="text-5xl font-bold text-center">Meet Our Mentors</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+        {tutors.map((tutor, index) => (
+          <div key={index} className="card bg-base-100  shadow-xl">
+            <figure>
+              <img
+                src={tutor.photo}
+                alt="photo"
+              />
+            </figure>
+            <div className="card-body items-center">
+              <h2 className="card-title font-bold">{tutor.name}</h2>
+              <p>{tutor.email}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TutorSection;
